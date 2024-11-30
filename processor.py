ab6e1378c1e1aa0baa4cdb0ddccd8987a487bb1b
Jithin J <jithinjayan1993@gmail.com>
2024-11-30 14:19:09 +0530

    Bug Fixes and intendation fixes added

--- processor.py
+++ processor.py
@@ -1,6 +1,7 @@
 import os
 import dask.dataframe as dd
 from transformers.continuous_data_transformer import ContinuousDataTransformer
+import numpy as np
 def read_input_data(input_parquet_dir, column_name):
 	"""
 	    Reads input data from a directory containing Parquet files, selects the specified column,
@@ -15,10 +16,11 @@ def read_input_data(input_parquet_dir, column_name):
     """
 	ddf = dd.read_parquet(input_parquet_dir, columns=[column_name])
 	ddf = ddf.repartition(partition_size="50MB")
+	return ddf
 
 def process_partition(partition, partition_idx):
-	 """
-	    Processes a single partition of data: fits the DPGMM model, transforms data,
+	"""
+		Processes a single partition of data: fits the DPGMM model, transforms data,
 	    and performs inverse transformation. Saves actual, transformed, and result data
 	    as NumPy arrays for later evaluation.
 
@@ -29,21 +31,21 @@ def process_partition(partition, partition_idx):
 	    Returns:
 	        tuple: Paths to the saved actual, transformed, and result data files.
     """
-    transformer = ContinuousDataTransformer(n_clusters=10, eps=0.005)
-    partition_np = partition.to_numpy().astype(float)  # Ensure data type is float for processing
-    transformer.fit(partition_np)
+	transformer = ContinuousDataTransformer(n_clusters=10, eps=0.005)
+	partition_np = partition.to_numpy().astype(float)  # Ensure data type is float for processing
+	transformer.fit(partition_np)
 
-    # Transform and inverse transform the data
-    transformed_data = transformer.transform(partition_np)
-    inv_transform_data = transformer.inverse_transform(transformed_data)
+	# Transform and inverse transform the data
+	transformed_data = transformer.transform(partition_np)
+	inv_transform_data = transformer.inverse_transform(transformed_data)
 
-    actual_path = os.path.join("actual_data", f"partition_{partition_idx}.npy")
-    np.save(actual_path, partition_np)
+	actual_path = os.path.join("actual_data", f"partition_{partition_idx}.npy")
+	np.save(actual_path, partition_np)
 
-    transformed_path = os.path.join("transformed_data", f"partition_{partition_idx}.npy")
-    np.save(transformed_path, transformed_data)
+	transformed_path = os.path.join("transformed_data", f"partition_{partition_idx}.npy")
+	np.save(transformed_path, transformed_data)
 
-    result_path = os.path.join("result_data", f"partition_{partition_idx}.npy")
-    np.save(result_path, inv_transform_data)
+	result_path = os.path.join("result_data", f"partition_{partition_idx}.npy")
+	np.save(result_path, inv_transform_data)
 
-    return actual_path, transformed_path, result_path
+	return actual_path, transformed_path, result_path
--- synthetic_data_generator.py
+++ synthetic_data_generator.py
@@ -1,7 +1,9 @@
 import pandas as pd
+import numpy as np
+import os
 
-def generate_chunk(chunk_id):
-     """
+def generate_chunk(chunk_id, current_count, replications_per_chunk, data):
+	"""
 	    Generate a synthetic data chunk with replicated rows and added noise.
 	    
 	    Args:
@@ -9,12 +11,12 @@ def generate_chunk(chunk_id):
 	    
 	    Returns:
 	        pd.DataFrame: Synthetic data for the chunk.
-    """
-    noise = np.random.normal(0, 0.01, size=(current_count * replications_per_chunk,))
-    synthetic_data = np.tile(price_data, replications_per_chunk) + noise
-    return pd.DataFrame({'Amount': synthetic_data})
+	"""
+	noise = np.random.normal(0, 0.01, size=(current_count * replications_per_chunk,))
+	synthetic_data = np.tile(data, replications_per_chunk) + noise
+	return pd.DataFrame({'Amount': synthetic_data})
 
-def generate_synthetic_data(existing_file_path, column_name, output_dir, target_count, num_chunks = 1000000):
+def generate_synthetic_data(existing_file_path, column_name, output_dir, target_count, chunk_size = 1000000):
 	"""
 		Generate synthetic data by replicating existing data and adding random noise, and save the result in chunks as Parquet files.
 
@@ -23,10 +25,10 @@ def generate_synthetic_data(existing_file_path, column_name, output_dir, target_
 		    column_name (str): Name of the column in the CSV file to use for data replication.
 		    output_dir (str): Directory to save the generated synthetic data files.
 		    target_count (int): Total number of rows to generate in the synthetic dataset.
-		    num_chunks (int, optional): Number of chunks to split the generated data into. Defaults to 1,000,000.
+		    chunk_size (int, optional): Size of each chunk. Defaults to 1,000,000.
 
 		Returns:
-    None: Saves the synthetic data chunks directly to the specified output directory.
+			None: Saves the synthetic data chunks directly to the specified output directory.
 	"""
 	os.makedirs(output_dir, exist_ok=True)
 
@@ -38,7 +40,7 @@ def generate_synthetic_data(existing_file_path, column_name, output_dir, target_
 	# Generate and save data in chunks
 	for i in range(target_count // chunk_size):
 		print(f"Generating chunk {i + 1}...")
-    	chunk_df = generate_chunk(i)
+		chunk_df = generate_chunk(i, current_count, replications_per_chunk, data)
 
-    	output_path = os.path.join(output_dir, f"synthetic_data_part_{i + 1}.parquet")
-    	chunk_df.to_parquet(output_path, engine="pyarrow", index=False)
+		output_path = os.path.join(output_dir, f"synthetic_data_part_{i + 1}.parquet")
+		chunk_df.to_parquet(output_path, engine="pyarrow", index=False)
